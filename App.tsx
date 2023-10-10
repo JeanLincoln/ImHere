b833b9457b24caddeef664f2d3e6d25b7f674ed6
Jean Lincoln <jeanlincol@gmail.com>
2023-10-10 19:24:29 -0300

    Organizando o projeto

--- App.tsx
+++ App.tsx
@@ -1,28 +1,6 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import Home from "./src/screens/Home";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do evento</Text>
-      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
-    </View>
-  );
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "gray",
-    padding: 24,
-  },
-
-  eventName: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#FDFCFE",
-    marginTop: 48,
-  },
-
-  eventDate: { color: "black", fontSize: 20 },
-});
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "gray",
+    padding: 24,
+  },
+
+  eventName: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#FDFCFE",
+    marginTop: 48,
+  },
+
+  eventDate: { color: "black", fontSize: 20 },
+});
