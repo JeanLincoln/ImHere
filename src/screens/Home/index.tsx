9707a9f3d62b13f651c7d510c067e60d84d4c628
Jean Lincoln <jeanlincol@gmail.com>
2023-10-10 19:28:04 -0300

    export e export default

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Text, View } from "react-native";
 import { styles } from "./styles";
 
-export default function Home() {
+export function Home() {
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
